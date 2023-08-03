c7242a2e642f555b6133df9913c2e3404d12186d
Harry Wade <harrywade1994@googlemail.com>
2023-08-03 15:43:18 +0100

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Configuration settings for the application
-
-CONFIG = {
-    "openai_api_key": "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",
-    "porcupine_access_key": "w6OczmoMGYbWhLYZHgBzctULcMgQcZkq67x1dWQ+EOMS9AvYImML2Q==",
-    "temp_audio_file_location": "C:/Users/harry/Documents/GitHub/Gizmo/temp/temp.mp3",
-    }
