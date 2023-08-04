df7361987963c38fee1f24285eaba0a5cc043cb2
Harry Wade <harrywade1994@googlemail.com>
2023-08-04 13:33:21 +0100

    Create __init__ template.py

---
+++ config/__init__ template.py
@@ -0,0 +1,8 @@
+# Configuration settings for the application
+
+CONFIG = {
+    "openai_api_key": "xxx",
+    "porcupine_access_key": "xxx",
+    "temp_audio_file_location": "path/to/temp/temp.mp3",
+    "porcupine_keyword_path": "path/to/HeyGizmo_VER.ppn"
+}
