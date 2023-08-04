42016e9b9f3efb7147a9343d422be43907436e6e
Harry Wade <55249313+lolscourge@users.noreply.github.com>
2023-08-04 13:31:14 +0100

    Delete config directory

--- config/__init__ template.py
+++
@@ -1,8 +0,0 @@
-# Configuration settings for the application
-
-CONFIG = {
-    "openai_api_key": "xxx",
-    "porcupine_access_key": "xxx",
-    "temp_audio_file_location": "path/to/tempfilelocation/temp.mp3",
-    "porcupine_keyword_path": "path/to/Gizmo_VER.ppn"
-}
